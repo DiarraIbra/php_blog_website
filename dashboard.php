d14024534cf1e0751f27c5fd7321266980268ab5
DiarraIbra <ibrahimadiarraibou@gmail.com>
2025-02-20 16:45:04 +0100

    addcategory commit

--- dashboard.php
+++ dashboard.php
@@ -4,5 +4,5 @@ session_destroy();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
 } else {
-    echo "Welcome to dashboard your name is : {$_SESSION['user_name']} and your role is as : {$_SESSION['user_role']}<a href='lohout.php'>Log Out</a>";
+    echo "Welcome to dashboard your name is : {$_SESSION['user_name']} and your role is as : {$_SESSION['user_role']}<a href='logout.php'> Log Out</a>";
 }
